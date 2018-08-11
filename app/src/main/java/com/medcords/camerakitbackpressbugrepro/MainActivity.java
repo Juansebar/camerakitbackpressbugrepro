e4211d995747081bbcee0adf795166044db930f1
prasad.pawar@medcords.com <prasad.pawar@medcords.com>
2018-08-11 14:11:43 +0530

    inital commit

---
+++ app/src/main/java/com/medcords/camerakitbackpressbugrepro/MainActivity.java
@@ -0,0 +1,52 @@
+package com.medcords.camerakitbackpressbugrepro;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.camerakit.CameraKitView;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MainActivity extends AppCompatActivity {
+    private CameraKitView cameraKitView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fresco.initialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
+        cameraKitView = findViewById(R.id.cameraKitView);
+    }
+
+    public void onCaptureButtonClicked(View view) {
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+                Log.i("MainActivity", "Image captured");
+            }
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 400);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraKitView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        cameraKitView.onPause();
+        super.onPause();
+    }
+}
